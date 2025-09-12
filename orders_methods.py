0b8cdbd82ee3d76055a1bfa1248761301ff79226
Hanna Radzevich <hradzevich@yandex.com>
2025-09-12 11:53:18 +0200

    Добавлен файл с тестами для получения списка заказов test_get_list_of_orders.py

--- orders_methods.py
+++ orders_methods.py
@@ -1,5 +1,6 @@
 import requests
 import allure
+import json
 from urls import *
 
 
@@ -12,3 +13,17 @@ class OrderMethods:
         # if create_order_response.status_code == 201:
         #     track = create_order_response.json()["track"]
         return create_order_response
+
+    @staticmethod
+    @allure.step("Получение списка заказов с возможной фильтрацией")
+    def get_list_of_orders(courierId=None, nearestStation=None, limit=30, page=0):
+        params = {}
+        if courierId is not None:
+            params["courierId"] = courierId
+        if nearestStation is not None:
+            params["nearestStation"] = json.dumps(nearestStation)
+        params["limit"] = limit
+        params["page"] = page
+
+        get_orders_response = requests.get(GET_LIST_OF_ORDERS, params=params)
+        return get_orders_response
